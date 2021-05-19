bba99b1e83d997a7844d5da5674d14d9262ec658
GolfPapaEcho <michaelghaleccc@gmail.com>
2021-05-19 17:03:13 +0100

    Added PsuedoRamdomChoice generator

---
+++ PsuedoRamdomChoice.py
@@ -0,0 +1,11 @@
+import random
+
+print("Random choice generator. Press Ctrl + C to exit.")
+
+while True:
+    input("\nPress Enter to generate choice.")
+    randNumber = random.randrange(1000)
+    if((randNumber % 2) == 0):
+        print("\nSolution")
+    else:
+        print("\nWater")
