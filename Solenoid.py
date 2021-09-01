01259255ca6ab9754a716b29629352c5173ed8d6
Michael G. Hale <michaelghaleccc@gmail.com>
2021-09-01 11:35:33 +0100

    added argparse feature

--- Solenoid.py
+++ Solenoid.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import argparse as argp
 
 GPIO.setmode(GPIO.BCM)
 
@@ -10,16 +11,19 @@ GPIO.setup(solenoid, GPIO.OUT)
 GPIO.setup(solenoidOnOff, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #sleep(120)#to allow Photometer parameters to be entered
-
-solenoidFrequency = 0
-while (int(solenoidFrequency) == 0):
-    solenoidFrequency = raw_input('\nPlease input solenoid frequency in Hz?:\n')
+parser = argp.ArgumentParser(description='Input frequency in Hertz then duty cycle as percentage.')
+parser.add_argument('--Frequency', type=float, required=True)
+parser.add_argument('--DutyCycle', type=float, required=True)
+args = parser.parse_args()
+solenoidFrequency = args.Frequency
+#while (int(solenoidFrequency) == 0):
+#    solenoidFrequency = raw_input('\nPlease input solenoid frequency in Hz?:\n')
     
 period = float(1.0/int(solenoidFrequency))
 
-dutyCyclePercent = 0
-while (int(dutyCyclePercent) == 0):
-    dutyCyclePercent = raw_input('\nPlease enter duty cycle percentage in range 1 to 100:\n')
+dutyCyclePercent = args.DutyCycle
+#while (int(dutyCyclePercent) == 0):
+#    dutyCyclePercent = raw_input('\nPlease enter duty cycle percentage in range 1 to 100:\n')
 dutyCyclePercent = float(dutyCyclePercent)
 dutyCycle = float(dutyCyclePercent*period/100)
 
