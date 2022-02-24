21fedc6d59b620d943e1f865b894e7c7f198e09c
GolfPapaEcho <mghgpe@gmail.com>
2022-02-24 15:42:41 +0000

    Added CSFCamera.py

---
+++ CSFCamera.py
@@ -0,0 +1,11 @@
+import datetime;
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+
+for filename in camera.capture_continuous('Pictures/img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+	print ('Capured %s' % filename)
+	sleep(18)
