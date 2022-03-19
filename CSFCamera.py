b910e124fe1aa8a53bf1570d22b96ea4e918dbfb
GolfPapaEcho <mghgpe@gmail.com>
2022-03-19 15:43:22 +0000

    Fixed filename bug

--- CSFCamera.py
+++ CSFCamera.py
@@ -3,9 +3,9 @@ from time import sleep
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.start_preview()
+#camera.start_preview()
 sleep(2)
 
-for filename in camera.capture_continuous('Pictures/img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+for filename in camera.capture_continuous('/home/pi/Pictures/img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
 	print ('Capured %s' % filename)
 	sleep(18)
