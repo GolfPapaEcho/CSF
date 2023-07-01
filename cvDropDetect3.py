1b777c7f8e9f9530349d789ce7c5015164180039
GolfPapaEcho <michaelghaleccc@gmail.com>
2023-07-01 15:36:52 +0100

    got upload2.py working

---
+++ cvDropDetect3.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import warnings
+import datetime
+import imutils
+import json
+import time
+import cv2
+import csv
+import os
+import pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True,
+	help="path to the JSON configuration file")
+args = vars(ap.parse_args())
